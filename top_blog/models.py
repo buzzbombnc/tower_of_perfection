91b326f24e97455e89788d18606c24df0fb3630c
buzzbombnc <buzzbomb-github@buzzbomb.net>
2016-02-11 18:41:11 -0500

    Add markdown formatting to the model and project.

--- top_blog/models.py
+++ top_blog/models.py
@@ -3,6 +3,17 @@ from django.conf import settings
 from django.utils import timezone
 from django.utils.text import slugify
 
+# Markdown and the defaults.
+import markdown
+
+markdown_defaults = {
+    'extensions': ['markdown.extensions.extra', 'markdown.extensions.sane_lists'],
+    'output_format': 'html5',
+    'lazy_ol': False,
+}
+
+# TODO: Move this to settings?
+
 class PublishedBlog(models.Manager):
     def get_queryset(self):
         return super(PublishedBlog, self).get_queryset().filter(published=True)
@@ -42,6 +53,9 @@ class Blog(models.Model):
 
         # Call the parent method.
         super(Blog, self).save(*args, **kwargs)
+
+    def formatted_article(self):
+        return markdown.markdown(self.article, **markdown_defaults)
 
     class Meta:
         ordering = ['-publish_date']
