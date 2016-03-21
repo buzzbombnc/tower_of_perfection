b8b2970cf0a8175caa870428f0f42d3061f5d914
buzzbombnc <buzzbomb-github@buzzbomb.net>
2016-03-21 19:39:20 -0400

    Add JS footnote tooltip creation routine.

--- top_blog/static/js/blog_entry.js
+++ top_blog/static/js/blog_entry.js
@@ -3,3 +3,34 @@
  */
 
 'use strict';
+
+function build_footnotes() {
+    var footnotes = $('div.footnote ol li')
+    if (footnotes.length > 0) {
+        footnotes.each(function(idx) {
+            // Copy the entire footnote.
+            var note = $(this).clone();
+            // Find the 'jump back' link.
+            var backlink = note.find('a.footnote-backref');
+            if (backlink.length === 1) {
+                // Grab the anchor by ID.
+                var link = $(backlink.attr('href'));
+                if (link.length === 1) {
+                    // Whack the backlink, create the tool tip and prevent the default click action.
+                    backlink.remove()
+                    link.tooltip({
+                        html: true,
+                        title: note.html()
+                    });
+                    link.click(function(event) {
+                        event.preventDefault();
+                    });
+                }
+            }
+        })
+    }
+}
+
+$(document).ready(function() {
+    build_footnotes();
+});
