b6c6768e8a8269c270c1a27c33a1ad6d5e720e61
buzzbombnc <buzzbomb-github@buzzbomb.net>
2016-04-11 21:42:00 -0400

    Tweak admin display of blog.

--- top_blog/admin.py
+++ top_blog/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 import models
 
+def article_size(obj):
+    return len(obj.article)
+article_size.short_description = 'Article Size'
+
 @admin.register(models.Blog)
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         'slug': ('title',)
     }
+    list_display = ('title', article_size, 'published', 'publish_date', 'update_date')
+    list_filter = ('published', 'publish_date')
+    save_as = True
 
