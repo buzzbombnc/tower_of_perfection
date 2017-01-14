129ab1bc1b3e3da3a5daded9af18765e5e15640f
buzzbombnc <buzzbomb-github@buzzbomb.net>
2017-01-14 18:30:01 -0500

    Add image count to admin list.

--- top_blog/admin.py
+++ top_blog/admin.py
@@ -5,6 +5,10 @@ def article_size(obj):
     return len(obj.article)
 article_size.short_description = 'Article Size'
 
+def image_count(obj):
+    return obj.blogimages_set.count()
+image_count.short_description = 'Images'
+
 class BlogImagesInline(admin.StackedInline):
     model = models.BlogImages
     verbose_name_plural = verbose_name = "Blog Images"
@@ -15,7 +19,7 @@ class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         'slug': ('title',)
     }
-    list_display = ('title', article_size, 'published', 'publish_date', 'update_date')
+    list_display = ('title', article_size, image_count, 'published', 'publish_date', 'update_date')
     list_filter = ('published', 'publish_date')
     save_as = True
     fields = ('author', ('title', 'slug'), ('publish_date', 'update_date'), 'published', 'article')
