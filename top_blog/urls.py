359a871b34e7a120578bb6083a65a61bf25881ed
buzzbombnc <buzzbomb-github@buzzbomb.net>
2017-01-17 19:48:01 -0500

    Update blog urls to work properly with PKs and slugs.

--- top_blog/urls.py
+++ top_blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import BlogIndex, BlogEntry
 
 urlpatterns = [
-    url(r'(?P<pk>[0-9]+)$', BlogEntry.as_view()),
-    url(r'(?P<slug>.+)$', BlogEntry.as_view(), name='blog_entry'),
-    url(r'', BlogIndex.as_view(), name='blog_index'),
+    url(r'^(?P<pk>[0-9]+)$', BlogEntry.as_view()),
+    url(r'^(?P<slug>[\w-]+)$', BlogEntry.as_view(), name='blog_entry'),
+    url(r'^$', BlogIndex.as_view(), name='blog_index'),
 ]
