59cafd104179aaf5024643c36b1a496b70091c00
buzzbombnc <buzzbomb-github@buzzbomb.net>
2017-01-31 22:11:57 -0500

    Add resume static page.

--- top_static/urls.py
+++ top_static/urls.py
@@ -3,4 +3,5 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^about$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^resume$', TemplateView.as_view(template_name='resume.html'), name='resume'),
 ]
